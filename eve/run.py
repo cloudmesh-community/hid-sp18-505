5a8a9d50b432b1aa821e6a6100f5bab145b4449a
Averill Cate, Jr <acatejr@gmail.com>
2018-02-01 07:19:45 -0700

    Added service update work.

--- eve/run.py
+++ eve/run.py
@@ -1,6 +1,19 @@
 from eve import Eve
+from flask import json, Response
 
 app = Eve()
 
+@app.route('/status', methods=['GET'])
+def status():
+    """ Endpoint that returns a status message. """
+    data = {
+        'status': 'OK'
+    }
+
+    packet = json.dumps(data)
+
+    resp = Response(packet, status=200, mimetype='application/json')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
