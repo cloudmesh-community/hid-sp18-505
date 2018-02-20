bbeec0964fcbf350d83f7673cff100722bc29354
Averill Cate, Jr <acatejr@gmail.com>
2018-02-20 07:19:38 -0700

    Started building client for swagger project.

---
+++ swagger/client/src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-block">
+          <h4 class="card-title">Card title</h4>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
