78d1c6bd7b8e8447b9689a9ee1e0050a80847496
Averill Cate, Jr <acatejr@gmail.com>
2018-03-02 17:00:04 -0700

    Altered test data a little.

--- project/cloudmesh/graphql/database.py
+++ project/cloudmesh/graphql/database.py
@@ -4,6 +4,6 @@ from models import Raingage
 connect('ccproject', host='mongomock://localhost', alias='default')
 
 def init_db():
-    for i in range(1, 15000):
+    for i in range(1, 150):
         rg = Raingage(name='rg{}'.format(i), code='code{}'.format(i))
         rg.save()
