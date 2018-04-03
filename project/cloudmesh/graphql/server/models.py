757df817d368f8a6602c3bc6c715f45eb276f9d7
Averill Cate, Jr <acatejr@gmail.com>
2018-04-02 19:38:34 -0700

    @laszewsk Update project paper.  Added outline/proposal.

--- project/cloudmesh/graphql/server/models.py
+++
@@ -1,7 +0,0 @@
-from mongoengine import Document
-from mongoengine.fields import (DateTimeField, ReferenceField, StringField)
-
-class Raingage(Document):
-    meta = {'collection': 'raingage'}
-    code = StringField()
-    name = StringField()
